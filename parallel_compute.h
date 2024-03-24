e4fef72b01a4a7f86246cd90f99530e66f26738d
omarelfouly <omarelfouly@aucegypt.edu>
2024-03-24 20:59:23 +0200

    Done, untested

--- parallel_compute.h
+++ parallel_compute.h
@@ -59,12 +59,27 @@ int parallel_compute(char *path, int n_proc, int (*f)(int, int)){
     // and return the result
     int *results = (int *)malloc(n_proc * sizeof(int));
     
-    // How should i pipe this?
-    // inside for loop or outside?
+    // create a pipe for each process
+    int **pipes = (int **)malloc(n_proc * sizeof(int *));
+    for (int i = 0; i < n_proc; i++)
+    {
+        pipes[i] = (int *)malloc(2 * sizeof(int));
+        pipe(pipes[i]);
+        // test if pipe was created successfully
+        if(pipes[i][0] == -1 || pipes[i][1] == -1){
+            printf("Error creating pipe\n");
+            return -1;
+        }
+    }
+
+
 
     for(int i = 0; i < n_proc; i++){
-        int pid = fork();
+        
+        pid_t pid = fork();
         if(pid == 0){
+            // close pipe read end
+            close(pipes[i][0]);
             // child process
             int start = parts[i];
             int end = (i == (n_proc - 1) )? count : (start + size);
@@ -74,13 +89,53 @@ int parallel_compute(char *path, int n_proc, int (*f)(int, int)){
                 result = f(result, numbers[j]);
             }
             // pass value to parent using pipe
+            write(pipes[i][1], &result, sizeof(int));
+            // close pipe write end
+            close(pipes[i][1]);
+            // exit child process
+            exit(0);
 
-
-
+        }else{
+            // close pipe write end
+            close(pipes[i][1]);
+            
         }
-
-
     
-}
+    }
 
+    // wait for all child processes to finish
+    while(1){
+        pid_t child = wait(NULL);
+        if(child == -1){
+            break;
+        }
+    }
+
+    // read results from pipes
+    for (int i = 0; i < n_proc; i++)
+    {
+        read(pipes[i][0], &results[i], sizeof(int));
+        close(pipes[i][0]);
+    }
+
+    // combine results
+    int finalResult = results[0];
+    for (int i = 1; i < n_proc; i++)
+    {
+        finalResult = f(finalResult, results[i]);
+    }
+
+    // free memory
+    free(numbers);
+    free(parts);
+    free(results);
+    for (int i = 0; i < n_proc; i++)
+    {
+        free(pipes[i]);
+    }
+    free(pipes);
+
+    return finalResult; 
+
+}
 #endif
