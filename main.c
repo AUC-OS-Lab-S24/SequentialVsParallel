fd374feee3358b48dfcc144cf097cd2fe8af8ded
Hussein <h.eazhary@gmail.com>
2024-03-26 00:15:54 +0200

    naming fix

--- main.c
+++ main.c
@@ -7,9 +7,9 @@ int main(){
   test_fix_nproc("fixed_nproc/", multiply, 1000000, "res_fixed_nproc2.txt");
   test_fix_nproc("fixed_nproc/", multiply, 1000000, "res_fixed_nproc3.txt");
 
-  test_fix_N("fix_n_res1", multiply, 10, "res_fixed_n.txt");
-  test_fix_N("fix_n_res2", multiply, 10, "res_fixed_n.txt");
-  test_fix_N("fix_n_res3", multiply, 10, "res_fixed_n.txt");
+  test_fix_N("fix_n_res1", multiply, 10, "res_fixed_n1.txt");
+  test_fix_N("fix_n_res2", multiply, 10, "res_fixed_n2.txt");
+  test_fix_N("fix_n_res3", multiply, 10, "res_fixed_n3.txt");
 
   return 0;
 }
