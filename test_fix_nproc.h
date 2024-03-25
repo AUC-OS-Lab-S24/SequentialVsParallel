32b45581fc6355c9137c60fe48bafb4ff7b94a0e
Ahmed-Waseem77 <ahmedwaseemr@aucegypt.edu>
2024-03-25 20:48:17 +0200

    xv6 compatible rand(), generate_n files

---
+++ test_fix_nproc.h
@@ -0,0 +1,20 @@
+
+#ifndef TEST_FIX_NPROC_H
+#define TEST_FIX_NPROC_H
+
+#include "parallel_compute.h"
+#include "sequential_compute.h"
+
+int test_fix_nproc()
+{
+
+
+
+
+    return 0;
+}
+
+
+
+
+#endif
